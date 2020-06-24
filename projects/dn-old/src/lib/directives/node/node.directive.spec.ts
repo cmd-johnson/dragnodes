86dfbe3eca5e360c29d0eed9518201cbddee3de5
Jonas Auer <jonas.auer.94@gmail.com>
2020-06-24 22:12:13 +0200

    Remove old dn implementation

--- projects/dn-old/src/lib/directives/node/node.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NodeDirective } from './node.directive';
-
-describe('NodeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NodeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/dn-old/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of dn
- */
-
-export * from './lib/components/graph/graph.component';
-export * from './lib/dn.module';
